b9bbf2dc42467f544ce4b3c0841876a53984566e
israelz330 <israelz@outlook.com>
2021-06-11 17:49:30 -0500

    First commit

---
+++ ConsoleUI/GreetingService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+//DE, Serilog, Settings
+
+namespace ConsoleUI
+{
+    partial class Program
+    {
+        public class GreetingService : IGreetingService
+        {
+            private readonly ILogger<GreetingService> _log;
+            private readonly IConfiguration _config;
+
+            public GreetingService(ILogger<GreetingService> log, IConfiguration config)
+            {
+                _log = log;
+                _config = config;
+            }
+            public void Run()
+            {
+                for (int i = 0; i < _config.GetValue<int>("LoopTimes"); i++)
+                {
+                    //This is #_
+                    _log.LogInformation("Run number { runNumber }", i);
+                }
+            }
+        }
+    }
+}
